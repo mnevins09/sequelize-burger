a47ca0d311ded304f370ed3794394d46815b502e
Mathieu Nevins <mnevins831@gmail.com>
2018-08-21 15:43:45 -0400

    config

--- models/burger.js
+++ models/burger.js
@@ -1,15 +1,14 @@
 module.exports = function (sequelize, DataTypes){
-  var Burger = sequelize.define("Burger", {
-    burger_name: {
-    	type: DataTypes.STRING(140),
-      allowNull: false,
-      setValues: ["Hamburger","Cheeseburger","Bacon Cheeseburger"]
-    },
-    devoured: {
-    	type: DataTypes.BOOLEAN,
-    	defaultValue: 0
-    }
-  });
-
-  return Burger;
-}
+    var Burger = sequelize.define("Burger", {
+      burger_name: {
+          type: DataTypes.STRING(140),
+        allowNull: false,
+      },
+      devoured: {
+          type: DataTypes.BOOLEAN,
+          defaultValue: 0
+      }
+    });
+  
+    return Burger;
+  }
